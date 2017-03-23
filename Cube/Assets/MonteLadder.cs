98023fac5358e3c1a1f621e4d07d0ef84764cde4
Bastien NIZARD <bnizard@e1r11p7.42.fr>
2017-03-23 17:24:45 +0100

    ok

---
+++ Cube/Assets/MonteLadder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MonteLadder : MonoBehaviour {
+
+	public bool Monte = false;
+	public float Vitesse = 8f;
+
+	void Update () {
+		if (Monte) 
+		{
+			GetComponent<CharacterController> ().transform.Translate (Vector3.up * Vitesse * Time.deltaTime);
+		}
+	}
+
+	void OnTriggerEnter(Collider Col)
+	{
+		if (Col.gameObject.tag == "Ladder") {
+			GetComponent<FirstPersonController> ().enabled = false;
+			Monte = true;
+		}
+	}
+
+	void OnTriggerExit(Collider Col)
+	{
+		if (Col.gameObject.tag == "Ladder") {
+			GetComponent<FirstPersonController> ().enabled = true;
+			Monte = false;
+		}
+	}
+}
